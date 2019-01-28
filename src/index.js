1db57f72ca905ff719034b5bec6cc447dbd91e15
Gustavo Novaes <gustavonovaes93@gmail.com>
2019-01-27 21:22:22 -0300

    Instala logger morgan

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 require('dotenv').config()
 
 const express = require('express')
+const morgan = require('morgan')
+
 const app = express()
 
+app.use(morgan('dev'))
 app.use(require('./routes'))
 
 app.listen(process.env.PORT || 3000)
