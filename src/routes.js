eb6d08aef3c3c32f130dba049f947f15669178e6
Gustavo Novaes <gustavonovaes93@gmail.com>
2019-01-27 22:59:50 -0300

    Cria rotas para listar, criar e deletar posts

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,34 @@
 const router = require('express').Router()
 
-router.get('/', (req, res) => {
-  return res.json({ hello: 'World' })
+const multerConfig = require('./config/multer')
+const multer = require('multer')(multerConfig)
+
+router.get('/posts', async (req, res) => {
+  const posts = await req.$models.Post.find()
+  return res.json(posts)
+})
+
+router.post('/posts', multer.single('file'), async (req, res) => {
+  const { originalname: name, size, filename: key } = req.file
+   
+  const post = await req.$models.Post.create({
+    name,
+    size,
+    key,
+    url: `/static/${key}`
+  })
+  
+  return res.json(post)
+})
+
+router.delete('/posts/:id', async (req, res) => {
+  const { id } = req.params
+
+  const post = await req.$models.Post.findById(id)
+
+  post.delete()
+
+  return res.send()
 })
 
 module.exports = router
