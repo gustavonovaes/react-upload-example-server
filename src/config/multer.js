e53121489eeaf939408a9e9a6d43fc64f2bb9ed2
Gustavo Novaes <gustavonovaes93@gmail.com>
2019-01-27 23:24:30 -0300

    Define fileFilter

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,5 +1,22 @@
 const { STATIC_FILES_PATH } = require('./constants')
 
+const allowedMimes = [
+  'image/jpeg',
+  'image/pjpeg',
+  'image/png',
+  'image/gif',
+]
+
 module.exports = {
   dest: STATIC_FILES_PATH,
+  fileFilter: (req, file, cb) => {
+    const isAllowedMime = allowedMimes.includes(
+      file.mimetype
+    )
+
+    cb(
+      isAllowedMime ? null : new Error('Invalid file type'),
+      true
+    )
+  }
 }
