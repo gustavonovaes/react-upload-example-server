f971c754b58d244e0310966fef6b64460097696a
Gustavo Novaes <gustavonovaes93@gmail.com>
2019-01-27 23:05:38 -0300

    Add middleware para remover arquivo em pre remove

--- src/schemas/Post.js
+++ src/schemas/Post.js
@@ -1,4 +1,6 @@
 const Schema = require('mongoose').Schema
+const { STATIC_FILES_PATH } = require('../config/constants')
+const { unlink } = require('../utils/fs')
 
 const Post = new Schema({
   name: String,
@@ -6,6 +8,12 @@ const Post = new Schema({
   key: String,
   url: String,
   createdAt: { type: Date, default: Date.now },
+})
+
+Post.pre('remove', function() {
+  const filePath = `${STATIC_FILES_PATH}/${this.key}`
+  console.log(filePath)
+  return unlink(filePath)
 })
 
 module.exports = Post
