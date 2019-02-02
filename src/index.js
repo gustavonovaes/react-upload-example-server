9a8d00e9ae597024294bbe0661e96121f5266bc7
Gustavo Novaes <gustavonovaes93@gmail.com>
2019-02-02 19:21:05 -0300

    Use cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 require('dotenv').config()
 
 const express = require('express')
+const cors = require('cors')
 const morgan = require('morgan')
 const { STATIC_FILES_PATH } = require('./config/constants')
 
 const app = express()
 
+app.use(cors())
+app.use(express.json())
 app.use(morgan('dev'))
 app.use(require('./plugins/mongoose')(process.env.MONGO_URL))
 app.use(require('./routes'))
