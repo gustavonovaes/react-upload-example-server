c8b189108c73166d8756b50a57c7d24f7a4998d4
Gustavo Novaes <gustavonovaes93@gmail.com>
2019-02-02 19:21:14 -0300

    Fix post key

--- src/routes.js
+++ src/routes.js
@@ -11,11 +11,13 @@ router.get('/posts', async (req, res) => {
 router.post('/posts', multer.single('file'), async (req, res) => {
   const { originalname: name, size, filename, key, location: url } = req.file
 
+  const postKey = key || filename
+
   const post = await req.$models.Post.create({
     name,
     size,
-    key: key || filename,
-    url: url || `/static/${key}`,
+    key: postKey,
+    url: url || `/static/${postKey}`,
     storageType: process.env.STORAGE_TYPE || 'local'
   })
 
